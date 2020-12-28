d9f6fcba86f402070461628935c001fb74b79aa5
Enrique <enriquecardenas96@gmail.com>
2020-12-28 13:32:38 -0800

    Add skills sections with icons

---
+++ src/components/SkillIcon.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { IconContext } from "react-icons";
+import { css } from "@emotion/core"
+
+const IconContainer = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  flex-wrap: wrap;
+  text-align: center;
+`
+
+export default (props) => {
+  if(props.icon){
+    const Icon = props.icon
+
+    return (
+      <div css={IconContainer}>
+        <IconContext.Provider value={{ size: "5em" }}>
+          <Icon />
+        </IconContext.Provider>
+        <p>{props.name}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div css={IconContainer}>
+      <img src={props.src} alt={props.alt} css={css` height: 5em; width: 5em;`}/>
+      <p>{props.name}</p>
+    </div>
+  )
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,18 @@
 import React from "react"
 import Layout from "../components/Layout/layout"
+import SkillIcon from "../components/SkillIcon"
+import { css } from "@emotion/core"
+
+import { FaHtml5, FaCss3, FaJs, FaReact, FaGitAlt, FaCogs, FaNode } from "react-icons/fa";
+import { SiPostgresql, SiGraphql, SiGatsby } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+import ResponsiveDesign from "../assets/icons/tablet-phone-and-browser.svg"
+
+
 
 export default () => (
   <Layout>
+    {console.log('responsive design', ResponsiveDesign)}
     <h1>About Me</h1>
     <p>
       I am a software engineer focusing on full stack development located in the Bay Area.
@@ -13,5 +23,24 @@ export default () => (
       I am an active volunteer at Code For San Francisco and am currently working on a Bay Area Relief Portal with the aim of helping small businesses and non-profits find
       financial support.
     </p>
+    <br />
+    <h2>Skills</h2>
+    <div css={css`display: flex; flex-wrap: wrap; justify-content: space-evenly;`}>
+      <SkillIcon icon={FaHtml5} name="HTML5"/>
+      <SkillIcon icon={FaCss3} name="CSS3" />
+      <SkillIcon src={ResponsiveDesign} alt="Responsive Design" name="Responsive Design"/>
+      <SkillIcon icon={FaJs} name="JavaScript" />
+      <SkillIcon icon={FaReact} name="React" />
+      <SkillIcon icon={FaGitAlt} name="Git" />
+    </div>
+    <div css={css`display: flex; flex-wrap: wrap; justify-content: space-evenly;`}>
+      <SkillIcon icon={FaCogs} name="RESTful APIs" />
+      <SkillIcon icon={FaNode} name="Node" />
+      <SkillIcon icon={GrMysql} name="MySQL" />
+      <SkillIcon icon={SiPostgresql} name="Postgres" />
+      <SkillIcon icon={SiGraphql} name="GraphQL" />
+      <SkillIcon icon={SiGatsby} name="Gatsby" />
+    </div>
+
   </Layout>
 )
