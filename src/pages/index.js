9e3cafda042ee411c3842343bc2ac2d3acddf42b
Enrique <enriquecardenas96@gmail.com>
2020-12-30 10:55:45 -0800

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,68 @@
-import React from "react"
-import { css } from "@emotion/core"
+import React, { useEffect, useRef } from "react"
 import Layout from "../components/Layout/layout"
-import BlogList from '../components/BlogList'
-import SEO from "../components/seo"
+import Projects from '../components/Projects'
+import SkillIcon from "../components/SkillIcon"
+import { Link } from "gatsby"
+
+import { css } from "@emotion/core"
+
+import { FaHtml5, FaCss3, FaJs, FaReact, FaGitAlt, FaCogs, FaNode } from "react-icons/fa";
+import { SiPostgresql, SiGraphql, SiGatsby } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+import ResponsiveDesign from "../assets/icons/tablet-phone-and-browser.svg"
 
 
-export default ({ data }) => {
+export default ({location}) => {
+  const commentRef = useRef();
+  useEffect( () => {
+    commentRef.current.innerHTML = `<!-- This icon by Freepik from "https://www.flaticon.com/" --> ${commentRef.current.innerHTML}`;
+  }, [] );
+
   return (
     <Layout>
-      <SEO />
-      <div>
-        <p>Hi, my name is Enrique and welcome to my website. </p>
-        
-        <h1
-          css={css`
-            display: inline-block;
-            border-bottom: 1px solid;
-          `}
-        >
-          Blog Posts
-        </h1>
-        <BlogList />
+      <p>Hi, my name is Enrique and welcome to my website. </p>
+      <br />
+
+      <h1>About Me</h1>
+      <p>
+        I am a software engineer focusing on full stack development located in the Bay Area.
+        I completed my B.S. in Computer Science with a minor in Math from San Jose State University.
+      </p>
+
+      <p>
+        I am currently working on a <Link to="/projects/bay-area-relief-portal/">web portal</Link> with the aim of helping small businesses and non-profits, in the San Francisco Bay Area,
+        find financial support.
+      </p>
+      <br />
+      <h2>Skills & Tools</h2>
+      <p>Here are some of the skills, tools, and technologies I've been using a lot recently.</p>
+      <div css={css`display: flex; flex-wrap: wrap; justify-content: space-evenly;`}>
+        <SkillIcon icon={FaHtml5} name="HTML5"/>
+        <SkillIcon icon={FaCss3} name="CSS3" />
+        <SkillIcon src={ResponsiveDesign} 
+          alt="Responsive Design" 
+          name="Responsive Design" 
+          ref={commentRef}
+        />
+        <SkillIcon icon={FaJs} name="JavaScript" />
+        <SkillIcon icon={FaReact} name="React" />
+        <SkillIcon icon={FaGitAlt} name="Git" />
       </div>
+      <div css={css`display: flex; flex-wrap: wrap; justify-content: space-evenly;`}>
+        <SkillIcon icon={FaCogs} name="RESTful APIs" />
+        <SkillIcon icon={FaNode} name="Node" />
+        <SkillIcon icon={GrMysql} name="MySQL" />
+        <SkillIcon icon={SiPostgresql} name="Postgres" />
+        <SkillIcon icon={SiGraphql} name="GraphQL" />
+        <SkillIcon icon={SiGatsby} name="Gatsby" />
+      </div>
+      <br />
+      <h2>Web Projects</h2>
+      <p>
+      Here are some the things I've been working on recently.
+      </p>
+      <Projects />
+    
     </Layout>
   )
 }
